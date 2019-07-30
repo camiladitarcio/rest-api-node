63bdda080b3b5ee0fd4b1814d09a4d7b11de86d6
puesto12 <puesto12@admins-iMac.local>
2019-07-30 17:53:17 -0300

    swagger added

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const bodyParser = require("body-parser");
 const carsRoutes = require("./routes/car");
 const clientRoutes = require("./routes/client");
 const authRoutes = require('./routes/auth');
+const swaggerFile = require('./docs/swagger.json');
+const swaggerDoc = require('swagger-ui-express');
 const middleware = require('./middleware');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -11,6 +13,9 @@ app.use(bodyParser.json());
 app.use('/car',middleware.checkToken, carsRoutes);
 app.use('/client', clientRoutes);
 app.use('/auth', authRoutes);
+app.use('/docs', swaggerDoc.serve, swaggerDoc.setup(swaggerFile));
+
+app.get('', (req, res) => res.send('Welcome to Ditar Api, Enjoy!!!!!!!!!!'))
 
 const port = process.env.PORT || 5000;
 
