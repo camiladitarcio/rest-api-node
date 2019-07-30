2c1eb73cf2c8e4c8c3ff8d810a71d96bf662332b
puesto12 <puesto12@admins-iMac.local>
2019-07-30 17:11:24 -0300

    database connection changed for heroku postgres

--- db/index.js
+++ db/index.js
@@ -1,8 +1,12 @@
-const { Client } = require("pg");
+const { Client } = require('pg')
 const client = new Client({
-  connectionString: "postgresql://localhost/cars-app"
-});
-
+  host: 'ec2-107-21-216-112.compute-1.amazonaws.com',
+  database: 'de5d2d428sfkt',
+  port: 5432,
+  user: 'vdlqesyvuemkyd',
+  password: '2956b0193165c70b3ebd22cc16463e901a38f407c20474f96da2202519710f37',
+  ssl: true
+})
 client.connect();
 
 module.exports = client;
